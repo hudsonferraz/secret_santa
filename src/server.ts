58300b4cf06600122f7674092731818c7b5191e6
Hudson Oliveira <hudson.oliveira@funttastic.com>
2026-06-24 08:31:45 -0300

    Initialize project with TypeScript, Express, and Nodemon setup. Added configuration files (.gitignore, nodemon.json, tsconfig.json) and created server entry point (src/server.ts). Included package.json and package-lock.json for dependencies management.

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import https from "https";
+import http from "http";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const runServer = (port: number, server: http.Server) => {
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+const regularServer = http.createServer(app);
+if (process.env.NODE_ENV === "production") {
+  //TODO: configurar SSL
+  //TODO: rodar server na 80 e na 443
+} else {
+  const serverPort: number = process.env.PORT
+    ? parseInt(process.env.PORT)
+    : 9000;
+  runServer(serverPort, regularServer);
+}
