176f5ce2678c681a3935f80042ba8d21bf0b10fb
Hudson Ferraz <hudsonferrazmoc@gmail.com>
2026-06-24 09:14:08 -0300

    Update TypeScript configuration to disable verbatim module syntax. Implement request interceptor middleware and add site routes with a ping endpoint.

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,19 @@ import "dotenv/config";
 import cors from "cors";
 import https from "https";
 import http from "http";
+import siteRoutes from "./routes/site";
+import { requestIntercepter } from "./utils/requestIntercepter";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.all("*", requestIntercepter);
+
+// app.use("/admin", adminRoutes);
+app.use("/", siteRoutes);
 
 const runServer = (port: number, server: http.Server) => {
   server.listen(port, () => {
