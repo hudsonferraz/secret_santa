1df0fae17c672e5e99f697640c0b77641dfe6d09
Hudson Ferraz <hudsonferrazmoc@gmail.com>
2026-06-26 16:53:53 -0300

    Add event management functionality
    - Created event controller with CRUD operations: getAll, getEvent, addEvent, updateEvent, and deleteEvent.
    - Implemented event service to interact with the database using Prisma.
    - Updated admin routes to include endpoints for event management, secured with authentication.
    - Utilized Zod for input validation in event creation and update processes.

---
+++ src/controllers/events.ts
@@ -0,0 +1,58 @@
+import { RequestHandler } from "express";
+import * as events from "../services/events";
+import { z } from "zod";
+
+export const getAll: RequestHandler = async (req, res) => {
+  const items = await events.getAll();
+  if (items) return res.json({ events: items });
+
+  res.json({ error: "Ocorreu um erro" });
+};
+
+export const getEvent: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  const eventItem = await events.getOne(Number(id));
+  if (eventItem) return res.json({ event: eventItem });
+
+  res.json({ error: "Ocorreu um erro" });
+};
+
+export const addEvent: RequestHandler = async (req, res) => {
+  const addEventSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    grouped: z.boolean(),
+  });
+  const body = addEventSchema.safeParse(req.body);
+  if (!body.success) return res.json({ error: "Dados inválidos" });
+
+  const newEvent = await events.add(body.data);
+  if (newEvent) return res.json({ event: newEvent });
+
+  res.json({ error: "Ocorreu um erro" });
+};
+
+export const updateEvent: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  const updateEventSchema = z.object({
+    status: z.boolean().optional(),
+    title: z.string().optional(),
+    description: z.string().optional(),
+    grouped: z.boolean().optional(),
+  });
+  const body = updateEventSchema.safeParse(req.body);
+  if (!body.success) return res.json({ error: "Dados inválidos" });
+
+  const updatedEvent = await events.update(Number(id), body.data);
+  if (updatedEvent) return res.json({ event: updatedEvent });
+
+  res.json({ error: "Ocorreu um erro" });
+};
+
+export const deleteEvent: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  const deletedEvent = await events.remove(Number(id));
+  if (deletedEvent) return res.json({ event: deletedEvent });
+
+  res.json({ error: "Ocorreu um erro" });
+};
